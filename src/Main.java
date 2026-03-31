fa427efef5816395c24333f7e37a0bf0a5f1b88d
pauluss-arch <paulussiagian227@gmail.com>
2026-03-31 14:42:52 +0700

    Pretest

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Mahasiswa mhsBaru = new Mahasiswa("Paulus Roberto Apolos Siagian", 19, "Ilmu Komputer");
+        
+    }
+}
